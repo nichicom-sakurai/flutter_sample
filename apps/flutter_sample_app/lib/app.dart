06d15dda9b8f7e2a3eec2ad83d98e4cde55db910
Sakurai Yuki <sakurai.yuki@nck.co.jp>
2024-03-14 08:19:42 +0900

    feat(app): Flavorによる環境ごとの切り替え

--- apps/flutter_sample_app/lib/app.dart
+++ apps/flutter_sample_app/lib/app.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class App extends StatelessWidget {
+class App extends ConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    const appName = String.fromEnvironment('appName');
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Text('Flutter Demo Home Page'),
+      home: const Text(appName),
     );
   }
 }
--- apps/flutter_sample_app/lib/main.dart
+++ apps/flutter_sample_app/lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:core_model/build_config.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'app.dart';
+import 'app_initializer.dart';
 
-void main() {
-  runApp(const App());
+Future<void> main() async {
+  final buildConfig = await AppInitializer.initialize();
+  runApp(
+    ProviderScope(
+      overrides: [
+        buildConfigProvider.overrideWithValue(buildConfig),
+      ],
+      child: const App(),
+    ),
+  );
 }
